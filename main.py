921f9879a09e70910587bf2dad52b805bfc25956
nikolainv <niccyper@mail.ru>
2024-09-12 14:08:43 +0000

    Нет лучше так

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ Am_lst=[]
 C_ost_lst=[]
 for i in range(8):
   Am = (C-L)/T
-  C_ost -= Am.subs({C: 30000, T:8, L:0})
-  Am_lst.append(round(Am.subs({C: 30000, T:8, L:0}), 2))
-  C_ost_lst.append(round(C_ost, 2))
+    C_ost_lst.append(round(C_ost, 2))
 print('Am_lst:', Am_lst)
 print('C_ost_lst:', C_ost_lst)

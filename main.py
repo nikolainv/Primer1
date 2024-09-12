48af10867c9314571cb45bb2cd31a6080e2fc574
nikolainv <niccyper@mail.ru>
2024-09-12 14:44:55 +0000

    Я поменял смотри!

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #Контейнер расчета
-from sympy import *
+from s
 k, T, C, L = symbols('k C T L')
 #1 способ
 C_ost=30000
 Am_lst=[]
-C_ost_lst=[]
+
 for i in range(8):
   Am = (C-L)/T
   C_ost -= Am.subs({C: 30000, T:8, L:0})
